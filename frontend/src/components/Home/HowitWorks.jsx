751fe064c9398fdb9686c4fc73942a044a21b2e0
aryapandeyy22 <aryapandeyy975@gmail.com>
2024-06-27 11:46:09 +0530

    4rggg

--- frontend/src/components/Home/HowitWorks.jsx
+++ frontend/src/components/Home/HowitWorks.jsx
@@ -3,6 +3,7 @@ import { FaUserPlus } from "react-icons/fa";
 import { MdFindInPage } from "react-icons/md";
 import { IoMdSend } from "react-icons/io";
 import './Home.css';
+import { Link, useNavigate } from "react-router-dom";
 
 const HowItWorks = () => {
   return (
@@ -11,20 +12,16 @@ const HowItWorks = () => {
         <div className="container">
           <h3>How FoundIt Works</h3>
           <div className="banner">
-            <div className="card">
+          <div className="card">
+              <Link to={"/login"} onClick={() => setShow(false)}>
               <FaUserPlus />
               <p>Create Account</p>
-              <p>
-               
-              </p>
-            </div>
-            <div className="card">
+              </Link>
+           </div>
+          <div className="card">
               <MdFindInPage />
               <p>Find a Job/Post a Job</p>
-              <p>
-               
-              </p>
-            </div>
+          </div>
             <div className="card">
               <IoMdSend />
               <p>Apply For Job/Recruit Suitable Candidates</p>
